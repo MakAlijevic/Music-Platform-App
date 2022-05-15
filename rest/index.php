efe144cc6db880681f517eb6e20c7b1525fda9f2
MakAlijevic <alijevic.mak@gmail.com>
2022-05-15 14:01:54 +0200

    added middleware security layer for login

--- rest/index.php
+++ rest/index.php
@@ -3,12 +3,39 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/services/UserService.class.php';
 require_once __DIR__.'/dao/UserDao.class.php';
 
 Flight::register('userDao','UserDao');
 Flight::register('userService', 'UserService');
+
+//middleware login method
+Flight::route('/*', function(){
+    $path = Flight::request()->url;
+    if($path == '/login'){
+        return TRUE;
+    }
+    $headers = getallheaders();
+    if(@!$headers['Authorization']){
+        Flight::json(["message" => "Unauthorized access"], 403);
+        return FALSE;
+    }else{
+        try {
+            $decoded = (array)JWT::decode($headers['Authorization'], new Key(Config::JWT_SECRET(), 'HS256'));
+            Flight::set('validUser', $decoded);
+            return TRUE;
+        } catch (\Exception $e) {
+            Flight::json(["message" => "Token authorization invalid"], 403);
+            return FALSE;
+        }
+    }
+
+    print_r($headers);
+});
 
 require_once __DIR__.'/routes/UserRoutes.php';
 
